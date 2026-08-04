ae34c9f85beea03e849a46676fdfd1d23601ff7a
Tenesh Chhantyal <tenishchhantyal22@gmail.com>
2026-08-04 18:03:16 +0545

    Change input validation and loop limits for Mario

--- Mario.c
+++ Mario.c
@@ -7,9 +7,9 @@ int main(void)
     {
         n = get_int("Positive Number: ");
     }
-    while(n < 0 || n > 8);
+    while(n < 1 || n > 8);
 
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n-i-1; j++)
         {
