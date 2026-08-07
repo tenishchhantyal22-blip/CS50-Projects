4b3dfadc8c62e194e1f594e23cdb6a8c9ee46325
Tenesh Chhantyal <tenishchhantyal22@gmail.com>
2026-08-07 15:44:46 +0545

    Implement readability calculator in Readability.c
    This program calculates the readability index of a given text based on the number of letters, words, and sentences.

---
+++ Readability.c
@@ -0,0 +1,50 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+");
+    }
+    else
+    {
+        printf("Grade %i", index);
+    }
+    printf("\n");
+}
